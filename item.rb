22f50430bf1f2879366bd8e813723bf443c32e81
lana <diegogarciaq98@gmail.com>
2023-01-16 15:08:36 -0500

    Add item.rb file with Item class

---
+++ item.rb
@@ -0,0 +1,22 @@
+require 'date'
+class Item
+  def initialize(genre, author, source, label, date)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = Date.parse(date)
+    @archived = false
+  end
+
+  def can_be_archived?
+    (Date.today - @publish_date) > 3652
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
+
+p Item.new('1', '1', '1', '1', '1990-12-11')
