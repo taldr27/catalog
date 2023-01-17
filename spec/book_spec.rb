52e8949d25e8145e48d7fe97f28810263d71cc9e
lana <diegogarciaq98@gmail.com>
2023-01-17 13:54:41 -0500

    Add test to label and book classes

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../src/book'
 describe 'Testing book' do
   context 'Creating a new book' do
-    new_book = Book.new('Publisher1', 'Bad', 'Fiction', 'Author', 'Gift', '1990-11-11')
+    new_book = Book.new('Publisher1', 'bad', 'Fiction', 'Author', 'Gift', '1990-11-11')
 
     it 'Check if is an instance of Book' do
       expect(new_book).to be_instance_of(Book)
@@ -17,6 +17,10 @@ describe 'Testing book' do
 
     it "Label of the created book should be 'Gift'" do
       expect(new_book.label).to eql 'Gift'
+    end
+
+    it "Can be archived method should be true'" do
+      expect(new_book.can_be_archived?).to eql true
     end
   end
 end
